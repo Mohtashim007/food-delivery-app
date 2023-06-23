6e3403bd391e6b95926a2574735ec3fa6ea5e733
Mohtashim007 <mohtashimtajamal007@gmail.com>
2023-06-23 18:21:24 +0500

    added the login and sign up functionality from front end side

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,64 @@
-import React from "react";
-import logo from "./asserts/logo.png";
+import React, { useState } from "react";
+import logo from "../asserts/logo.png";
+import { Link } from "react-router-dom";
+import { HiOutlineUserCircle } from "react-icons/hi";
+import { BsCartFill } from "react-icons/bs";
 
 function Header() {
+  const [showMenu, setShowMenu] = useState(false);
+  //   const userData = useSelector((state) => state.user);
+
+  const handleShowMenu = () => {
+    setShowMenu((preve) => !preve);
+  };
   return (
-    <header className="fixed shadow-md w-full h-16 px-2 md:px-4">
-      <div className="flex items-center h-full">
-        <div className="h-12">
-          <img src={logo} alt="" className="h-full " />
+    <header className="fixed shadow-md w-full h-16 px-2 md:px-4 z-50 bg-white">
+      <div className="flex items-center h-full justify-between">
+        <Link to="">
+          <div className="h-12">
+            <img src={logo} alt="" className="h-full " />
+          </div>
+        </Link>
+
+        <div className="flex items-center gap-4 md:gap-7">
+          <nav className="flex gap-4 md:gap-6 text-base md:text-lg">
+            <Link to="">Home</Link>
+            <Link to="menu">Menu</Link>
+            <Link to="about">About</Link>
+            <Link to="contact">Contact</Link>
+          </nav>
+
+          <div className="text-2xl text-slate-600 relative">
+            <BsCartFill />
+            <div className="absolute -top-1 -right-1 text-white bg-red-500 h-4 w-4 rounded-full m-0 p-0 text-sm text-center">
+              0
+            </div>
+          </div>
+          <div className="text-slate-600" onClick={handleShowMenu}>
+            <div className="text-3xl cursor-pointer w-8 h-8 rounded-full overflow-hidden drop-shadow-md">
+              {/* {userData.image ? (
+                <img src={userData.image} className="h-full w-full" />
+              ) : ( */}
+              <HiOutlineUserCircle />
+              {/* )} */}
+            </div>
+            {showMenu && (
+              <div className="absolute right-2 bg-white py-2 shadow drop-shadow-md flex flex-col min-w-[120px] text-center">
+                <Link
+                  className="whitespace-nowrap cursor-pointer px-2"
+                  to="newproduct"
+                >
+                  New Product
+                </Link>
+                <Link
+                  to="login"
+                  className="whitespace-nowrap cursor-pointer px-2"
+                >
+                  Login
+                </Link>
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </header>
--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Menu from "./pages/Menu";
+import Contact from "./pages/Contact";
+import Login from "./pages/Login";
+import NewProduct from "./pages/NewProduct";
+import SignUp from "./pages/SignUp";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index element={<Home />} />
+      <Route path="menu" element={<Menu />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="login" element={<Login />} />
+      <Route path="newproduct" element={<NewProduct />} />
+      <Route path="signup" element={<SignUp />} />
+    </Route>
+  )
 );
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<RouterProvider router={router} />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
